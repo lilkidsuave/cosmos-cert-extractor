e25ab150a788190feebcdf0831dd342854e1b185
lilkidsuave <66171643+lilkidsuave@users.noreply.github.com>
2024-06-20 10:03:34 -0400

    Update extract.py

--- extract.py
+++ extract.py
@@ -18,28 +18,15 @@ CONFIG_PATH = '/input/cosmos.config.json'
 CERT_PATH = '/output/certs/cert.pem'
 KEY_PATH = '/output/certs/key.pem'
 DEFAULT_CHECK_INTERVAL = 0  # Default check interval is when it expires
-
+curr_valid_until = None
 # Event to indicate interruption by signal
 interrupted = False
 lock = threading.Lock()
-current_config_hash = None
-
-def compute_relevant_config_hash(config_path):
-    # Compute the SHA-256 hash of the relevant parts of the config file
-    hasher = hashlib.sha256()
-    try:
-        with open(config_path, 'r') as f:
-            config = json.load(f)
-            relevant_data = json.dumps({
-                'TLSCert': config['HTTPConfig']['TLSCert'],
-                'TLSKey': config['HTTPConfig']['TLSKey']
-            }, sort_keys=True).encode('utf-8')
-            hasher.update(relevant_data)
-    except (OSError, json.JSONDecodeError) as e:
-        print(f'Error computing hash for config file: {e}')
-        return None
-    return hasher.hexdigest()
-
+class ConfigFileHandler(FileSystemEventHandler):
+    def on_modified(self, event):
+        if event.src_path == INPUT_PATH and os.path.getsize(event.src_path) > 0:
+            renew_certificates()
+            
 def get_local_timezone():
     # Get the system's local timezone from environment variable or tzlocal
     tz_name = os.getenv('TZ', get_localzone() )
@@ -92,24 +79,18 @@ def write_certificates(cert, key):
 def renew_certificates():
     # Renew the certificates by reading from the config file and writing to the certificate files.
     global interrupted
-    cert_data, key_data = load_certificates()
+    global curr_valid_until
     print('Updating certificates...')
     config_object = load_config()
     if config_object:
         cert = config_object['HTTPConfig']['TLSCert']
         key = config_object['HTTPConfig']['TLSKey']
-        write_certificates(cert, key)
-        print('Certificates updated.')
+        valid_until = config_object["HTTPConfig"]["TLSValidUntil"]
+        if valid_until != curr_valid_until:
+            write_certificates(cert, key)
+            curr_valid_until = valid_until
     else:
         print('Couldn\'t read the config file.')
-
-def is_cert_expired(cert_data, tz):
-    # Check if the certificate has expired and convert the expiry date to the specified timezone.
-    cert = crypto.load_certificate(crypto.FILETYPE_PEM, cert_data)
-    expiry_date_str = cert.get_notAfter().decode('ascii')
-    expiry_date = datetime.strptime(expiry_date_str, '%Y%m%d%H%M%SZ').replace(tzinfo=timezone.utc)
-    expiry_date = expiry_date.astimezone(tz)  # Convert to specified timezone
-    return expiry_date < datetime.now(tz), expiry_date  # Return expiry status and expiry date
 
 def get_check_interval():
     # Get the check interval from the environment variable or use the default.
@@ -134,20 +115,17 @@ def signal_handler(sig, frame):
     time.sleep(1)
 
 def main():
-    global current_config_hash
+    global curr_valid_until
     signal.signal(signal.SIGINT, signal_handler)  # Register SIGINT handler
     next_check_time = time.time()
     tz = get_local_timezone()  # Get the local timezone
     renew_certificates()  # Initial renewal of certificates
     watchdog_enabled = get_watchdog_status()  # Check if watchdog is enabled
-    current_config_hash = compute_relevant_config_hash(CONFIG_PATH)  # Compute initial hash
-    cert_data, key_data = load_certificates()
-    expired, expiry_date = is_cert_expired(cert_data, tz)
     print(f'New certificate expires on {expiry_date.isoformat()} {expiry_date.tzinfo}.')
 
     if watchdog_enabled:
         print('Watchdog enabled. Monitoring the configuration file for changes.')
-        event_handler = ConfigChangeHandler()
+        event_handler = ConfigFileHandler()
         observer = Observer()
         observer.schedule(event_handler, path=os.path.dirname(CONFIG_PATH), recursive=False)
         observer.start()
@@ -156,10 +134,9 @@ def main():
         interrupted = False
         check_interval = get_check_interval()  # Get the check interval
         current_time = time.time()
-        cert_data, key_data = load_certificates()
         # Condition to renew certificates if expired or interrupted
-        expired, expiry_date = is_cert_expired(cert_data, tz)
-        if expired and check_interval > 0:
+        valid_until = config_object["HTTPConfig"]["TLSValidUntil]
+        if valid_until != curr_valid_until: and check_interval > 0:
             old_expiry_date = expiry_date
             renew_certificates()
             expired, expiry_date = is_cert_expired(cert_data, tz)
@@ -170,7 +147,7 @@ def main():
             print(f'Updating again in {check_interval} seconds.')
             next_check_time = current_time + check_interval
         # Handle the case when CHECK_INTERVAL is 0 and certificate expired or interrupted
-        elif check_interval == 0 and expired:
+        elif check_interval == 0 and valid_until != curr_valid_until:
             old_expiry_date = expiry_date
             renew_certificates()
             expired, expiry_date = is_cert_expired(cert_data, tz)
